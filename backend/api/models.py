aa302c2a8dc00f17717f822da54deae8e91ce213
Stan1-0 <stankofb@gmail.com>
2024-07-16 15:56:30 +0000

    fixed merge conflict

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-<<<<<<< HEAD
 class React(models.Model):
     name = models.CharField(max_length=100),
     
-=======
->>>>>>> d1a3dba1b947b22085c4541ac30cb56550f8b40d
